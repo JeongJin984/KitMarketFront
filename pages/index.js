877f926df2937783bf66188c1db9df40a2e67fe8
20170534 <1gbae53@gmail.com>
2021-03-15 00:20:20 +0900

    login틀

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,6 @@
 import React, { useCallback } from 'react';
 
-import { Form, Button } from 'react-bootstrap'
+import { Row, Col, Button, Form, FormGroup, Label, Input, FormText, Card, CardTitle, CardText  } from 'reactstrap';
 
 import { useDispatch } from 'react-redux';
 import { LOGIN_REQUEST } from '../reducer/user';
@@ -25,27 +25,61 @@ const Home = () => {
 	) 
 
 	return (
-		<Form onSubmit={handleSubmit}>
-			<Form.Group controlId="formBasicUsername">
-				<Form.Label>Username</Form.Label>
-				<Form.Control type="text" placeholder="Enter username" />
-				<Form.Text className="text-muted">
-					We'll never share your email with anyone else.
-				</Form.Text>
-			</Form.Group>
-
-			<Form.Group controlId="formBasicPassword">
-				<Form.Label>Password</Form.Label>
-				<Form.Control type="password" placeholder="Password" />
-			</Form.Group>
-			<Form.Group controlId="formBasicCheckbox">
-				<Form.Check type="checkbox" label="Check me out" />
-			</Form.Group>
-			<Button variant="primary" type="submit">
-				Submit
-			</Button>
-		</Form>
-	)
+		<div>
+			<Row>
+				<Col sm="6">
+					<Card body>
+					<CardTitle tag="h5">로그인</CardTitle>
+					<CardText>ID와 Password를 입력해주세요.</CardText>
+					<Form className = "text-center">
+						<FormGroup row>
+							<Label className = "text-center" for="exampleEmail" sm={2}>ID</Label>
+							<Col sm={5}>
+							<Input type="email" name="email" id="exampleEmail" placeholder="ID" />
+							</Col>
+						</FormGroup>
+						<FormGroup row>
+							<Label className = "text-center" for="examplePassword" sm={2}>Password</Label>
+							<Col sm={5}>
+							<Input type="password" name="password" id="examplePassword" placeholder="password" />
+							</Col>
+						</FormGroup>
+						<FormGroup check row>
+							<Col sm={{ size: 5, offset: 1 }}>
+							<Button>Login</Button>
+							</Col>
+						</FormGroup>
+					</Form>
+					</Card>
+				</Col>
+				{/* <Col sm="6">
+					<Card body>
+					<CardTitle tag="h5">회원가입</CardTitle>
+					<CardText>ID와 Password를 입력해주세요.</CardText>
+					<Form className = "text-center">
+						<FormGroup row>
+							<Label className = "text-center" for="exampleEmail" sm={2}>ID</Label>
+							<Col sm={5}>
+							<Input type="email" name="email" id="exampleEmail" placeholder="ID" />
+							</Col>
+						</FormGroup>
+						<FormGroup row>
+							<Label className = "text-center" for="examplePassword" sm={2}>Password</Label>
+							<Col sm={5}>
+							<Input type="password" name="password" id="examplePassword" placeholder="password" />
+							</Col>
+						</FormGroup>
+						<FormGroup check row>
+							<Col sm={{ size: 5, offset: 1 }}>
+							<Button>Login</Button>
+							</Col>
+						</FormGroup>
+					</Form>
+					</Card>
+				</Col> */}
+			</Row>
+		</div>
+	  );
 }
 
 export default Home;
