e1ebbc976d1c0bd7fc07e77c8cfbd9a29e0b2640
20170534 <1gbae53@gmail.com>
2021-04-07 22:05:07 +0900

    모달창 내용

--- components/ModalButton.js
+++ components/ModalButton.js
@@ -2,7 +2,7 @@ import React, { useState, useEffect } from 'react';
 import Link from 'next/link';
 import styled from 'styled-components'
 
-import { Button, Modal, ModalHeader, ModalBody, ModalFooter } from 'reactstrap';
+import { Row, Col, Button, Modal, ModalHeader, ModalBody, ModalFooter,InputGroup, InputGroupAddon, InputGroupText, Input } from 'reactstrap';
 
 const ModalButton = () => {
   const [modal, setModal] = useState(false);
@@ -19,7 +19,7 @@ const ModalButton = () => {
         textAlign:'center',
         margin:'0',
         position:'absolute',
-        bottom:'-5%',
+        bottom:'-15%',
         right:'-5%'}}>
         <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.6.1/css/all.css" />
         <link rel="stylesheet" href="style.css" />
@@ -27,14 +27,89 @@ const ModalButton = () => {
           <i className="fas fa-pencil-alt" style={{color:'white'}}/>
         </a>
       </Button>
-      <Modal isOpen={modal} toggle={toggle}>
-        <ModalHeader toggle={toggle}>Modal title</ModalHeader>
+      <Modal size='lg' isOpen={modal} toggle={toggle}>
+        <ModalHeader toggle={toggle}>게시글 작성</ModalHeader>
         <ModalBody>
-          모달띄우기
+          <label tag="h3">제목</label>
+          <Input placeholder="제목" />
+          <br />
+          <Row>
+            <Col xs = '3'>
+              <label>구하는 인원</label>
+            </Col>
+            <Col xs='6'>
+              <Row>
+                <Col xs = '3'>
+                  <Input placeholder=""/>
+                </Col>
+                <Col xs = '3'>
+                  <label>명 중에 </label>
+                </Col>
+                <Col xs = '3'>
+                  <Input placeholder=""/>
+                </Col>
+                <Col xs = '3'>
+                  <label>명 구해요 </label>
+                </Col>
+              </Row>
+            </Col>
+          </Row>
+          <br />
+          <label>내용</label>
+          <Input type="textarea" name="text" id="exampleText" placeholder="내용"/>
+          <br />
+          <label>마감 날짜 & 시간</label>
+          <Row>
+            <Col xs = '2'>
+              <Input placeholder=""/>
+            </Col>
+            <Col xs = '1'>
+              <label>년</label>
+            </Col>
+            <Col xs = '2'>
+              <Input placeholder=""/>
+            </Col>
+            <Col xs = '1'>
+              <label>월</label>
+            </Col>
+            <Col xs = '2'>
+              <Input placeholder=""/>
+            </Col>
+            <Col xs = '1'>
+              <label>일</label>
+            </Col>
+          </Row>
+          <br />
+          <Row>
+            <Col xs = '3'>
+
+            </Col>
+            <Col xs = '2'>
+            <Input type="select" name="select" id="exampleSelect">
+              <option>AM</option>
+              <option>PM</option>
+            </Input>
+            </Col>
+            <Col xs = '2'>
+              <Input placeholder=""/>
+            </Col>
+            <Col xs = '1'>
+              <label>시</label>
+            </Col>
+            <Col xs = '2'>
+              <Input placeholder=""/>
+            </Col>
+            <Col xs = '1'>
+              <label>분</label>
+            </Col>
+            <Col xs = '1'>
+              <label>까지</label>
+            </Col>
+          </Row>
         </ModalBody>
         <ModalFooter>
-          <Button color="primary" onClick={toggle}>Do Something</Button>{' '}
-          <Button color="secondary" onClick={toggle}>Cancel</Button>
+          <Button outline color="secondary" onClick={toggle}>취소</Button>
+          <Button color="secondary" onClick={toggle}>확인</Button>{' '}
         </ModalFooter>
       </Modal>
     </div>
