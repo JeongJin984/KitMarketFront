8644347ae283f843c281a9ee7abba70e02a20c17
Chuhj <chj9574@naver.com>
2021-04-17 20:51:08 +0900

    create pagination function

--- components/Pagination.js
+++ components/Pagination.js
@@ -5,21 +5,50 @@ import { Button, ButtonToolbar, ButtonGroup } from 'reactstrap';
 const Pagination = () => {
   const { currentPage, maxPage } = useSelector((state) => state.post.meta);
   const router = useRouter();
+  // const currentPage = router.query.page - 1 || 0;
+  // const maxPage = 15;
+
   const page = Array(maxPage)
     .fill()
     .map((page, i) => i + 1);
+
+  const pageSetNum = parseInt(currentPage / 10);
+  const currentPageSet = page.slice(10 * pageSetNum, 10 * pageSetNum + 10);
+
   const { category } = router.query;
 
   const onClickPage = useCallback(
-    (i) => {
+    (num) => {
       if (category) {
-        router.push(`/board/${category}/?page=${i}`);
+        router.push(`/board/${category}/?page=${num}`);
       } else {
-        router.push(`/?page=${i}`);
+        router.push(`/?page=${num}`);
       }
     },
     [category]
   );
+
+  const onClickNext = useCallback(() => {
+    const nextPageNum = (pageSetNum + 1) * 10 + 1;
+    if (maxPage >= nextPageNum) {
+      router.push(`/?page=${nextPageNum}`);
+    }
+  }, [pageSetNum, maxPage]);
+
+  const onClickPrev = useCallback(() => {
+    const prevPageNum = pageSetNum * 10;
+    if (prevPageNum >= 1) {
+      router.push(`/?page=${prevPageNum}`);
+    }
+  }, [pageSetNum]);
+
+  const onClickNextEnd = useCallback(() => {
+    router.push(`/?page=${maxPage}`);
+  }, [maxPage]);
+
+  const onClickPrevEnd = useCallback(() => {
+    router.push(`/?page=1`);
+  }, []);
 
   return (
     <ButtonToolbar
@@ -28,13 +57,13 @@ const Pagination = () => {
       }}
     >
       <ButtonGroup>
-        <Button outline color="secondary">
+        <Button outline color="secondary" onClick={onClickPrevEnd}>
           ⟪
         </Button>
-        <Button outline color="secondary">
+        <Button outline color="secondary" onClick={onClickPrev}>
           ⟨
         </Button>
-        {page.map((i) => {
+        {currentPageSet.map((i) => {
           if (i - 1 === currentPage) {
             return (
               <Button key={i} color="primary" onClick={() => onClickPage(i)}>
@@ -54,10 +83,10 @@ const Pagination = () => {
             );
           }
         })}
-        <Button outline color="secondary">
+        <Button outline color="secondary" onClick={onClickNext}>
           ⟩
         </Button>
-        <Button outline color="secondary">
+        <Button outline color="secondary" onClick={onClickNextEnd}>
           ⟫
         </Button>
       </ButtonGroup>
