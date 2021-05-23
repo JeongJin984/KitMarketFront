1094419b8b0e89d3656de88c75b89ad2562ca102
Chuhj <chj9574@naver.com>
2021-05-23 19:09:23 +0900

    로그인페이지에서 loadUser X

--- pages/_app.js
+++ pages/_app.js
@@ -1,41 +1,38 @@
-import { wrapper } from '../store'
-import axios from 'axios'
+import { wrapper } from '../store';
+import axios from 'axios';
 import App from 'next/app';
 
-import 'bootstrap/dist/css/bootstrap.min.css'
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 import { loadUserRequest } from '../reducer/user';
 
 class MyApp extends App {
-	static getInitialProps = async ({Component, ctx}) => {
+  static getInitialProps = async ({ Component, ctx }) => {
+    const state = ctx.store.getState();
+    const cookie = ctx.req ? ctx.req.headers.cookie : '';
+    axios.defaults.headers.Cookie = '';
+    if (cookie) {
+      axios.defaults.headers.Cookie = cookie;
+    }
+    if (!state.user.me && !ctx.pathname === '/login') {
+      ctx.store.dispatch(loadUserRequest());
+    }
 
-		const state = ctx.store.getState()
-		const cookie = ctx.req ? ctx.req.headers.cookie : '';
-		axios.defaults.headers.Cookie = '';
+    return {
+      pageProps: {
+        // Call page-level getInitialProps
+        ...(Component.getInitialProps
+          ? await Component.getInitialProps(ctx)
+          : {}),
+        pathname: ctx.pathname,
+      },
+    };
+  };
 
-		if(cookie) {
-			axios.defaults.headers.Cookie = cookie
-		}
-		if(!state.user.me) {
-			ctx.store.dispatch(loadUserRequest())
-		}
-
-		return {
-			pageProps: {
-				// Call page-level getInitialProps
-				...(Component.getInitialProps ? await Component.getInitialProps(ctx) : {}),
-				pathname: ctx.pathname,
-			}
-		};
-	}
-
-	render() {
-			const {Component, pageProps} = this.props;
-			return (
-					<Component {...pageProps} />
-			);
-	}
-
+  render() {
+    const { Component, pageProps } = this.props;
+    return <Component {...pageProps} />;
+  }
 }
 
-export default wrapper.withRedux(MyApp)
+export default wrapper.withRedux(MyApp);
