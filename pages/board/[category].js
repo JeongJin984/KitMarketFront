810f1f05505d12804407bcfef157317639d7fec0
Chuhj <chj9574@naver.com>
2021-06-02 18:46:09 +0900

    dropdown

--- pages/board/[category].js
+++ pages/board/[category].js
@@ -1,22 +1,43 @@
 import React from 'react';
 import { useSelector } from 'react-redux';
+import { useRouter } from 'next/router';
 import { wrapper } from '../../store';
 import { END } from 'redux-saga';
 import axios from 'axios';
 
-import { Row } from 'reactstrap';
+import { Row, Input } from 'reactstrap';
 import PostCard from '../../components/PostCard';
 import JumbotronComponent from '../../components/JumbotronComponent';
 import AppLayout from '../../components/AppLayout';
 import WritePostModal from '../../components/WritePostModal';
 import BoardPagination from '../../components/BoardPagination';
-import {loadMainPostsRequest} from "../../data/event/postEvent";
+import {
+  loadMainPostsRequest,
+  searchPostsRequest,
+} from '../../data/event/postEvent';
 
 const Category = () => {
   const { mainPosts } = useSelector((state) => state.post);
+  const router = useRouter();
+  const { category } = router.query;
   return (
     <AppLayout>
       <JumbotronComponent />
+      <Input
+        style={{ width: 'min-content', marginLeft: 'auto' }}
+        type="select"
+        name="select"
+      >
+        <option onClick={() => router.push(`/board/${category}`)}>전체</option>
+        <option
+          onClick={() => router.push(`/board/${category}?status=POSTING`)}
+        >
+          모집중인 모임
+        </option>
+        <option onClick={() => router.push(`/board/${category}?status=CLOSED`)}>
+          모집 종료된 모임
+        </option>
+      </Input>
       <Row>
         {mainPosts.map((postInfo) => (
           <PostCard postInfo={postInfo} key={postInfo.id} />
@@ -34,15 +55,23 @@ export const getServerSideProps = wrapper.getServerSideProps(
   async ({ store, req, query }) => {
     const q = query.category || 'post';
     const category = `${q}List`;
+    const status = query.status;
     const page = query.page - 1 || 0;
-    const data = { category, page };
+    const { select, search } = query;
+
+    const data = { category, status, page };
     const cookie = req ? req.headers.cookie : '';
     axios.defaults.headers.Cookie = '';
     if (req && cookie) {
       axios.defaults.headers.Cookie = cookie; // SSR일 때만 쿠키를 넣어줌
     }
 
-    store.dispatch(loadMainPostsRequest(data));
+    if (search) {
+      store.dispatch(searchPostsRequest({ select, search, page }));
+    } else {
+      store.dispatch(loadMainPostsRequest(data));
+    }
+
     store.dispatch(END); // Request가 끝날 때 까지 기다려줌
     await store.sagaTask.toPromise();
   }
